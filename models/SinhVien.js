7044f0a9de2a5682758d85a4b3cc4ee86b1d7f45
danhNg <danh.nguyenthanh102@gmail.com>
2022-10-26 12:07:47 +0700

    es6_buoi_1

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -1,11 +1,12 @@
- function SinhVien() {
+function SinhVien() {
   this.maSinhVien = "";
   this.tenSinhVien = "";
 }
 
- const arr = [1,2,3,4];
+const arr = [1, 2, 3, 4];
+const domain = { url: "https://api.com" };
 
- export {arr, SinhVien}
+export { arr, SinhVien, domain };
 
 //export default: mỗi file chỉ thực hiện được một lệnh
 export default SinhVien;
